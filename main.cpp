80cecff467203555fb8224ea973437c7c2dd56b6
Keith Wade <keawade@protonmail.com>
2018-07-16 20:33:49 -0500

    Fix build errors

--- main.cpp
+++ main.cpp
@@ -3,29 +3,33 @@ http://www.multigesture.net/articles/how-to-write-an-emulator-chip-8-interpreter
 http://devernay.free.fr/hacks/chip8/C8TECH10.HTM
 */
 #include "chip8.h"
+#include <iostream>
+
+using namespace std;
 
 Chip8 emulator;
 
 int main(int argc, char **argv)
 {
-  // setupGraphics();
-  // setupInput();
 
-  // Intialize Chip-8 system
-  emulator.initialize();
+    // setupGraphics();
+    // setupInput();
 
-  // Load program into memory
-  emulator.loadProgram("programs/PONG");
+    // Initalize Chip-8 system
+    emulator.initialize();
 
-  for (;;)
-  {
-    // Emulate a cycle
-    emulator.emulateCycle();
+    // Load program into memory
+    emulator.loadProgram("programs/PONG");
 
-    // if (emulator.drawFlag)
-    //   drawGraphics();
-    
-    // emulator.setKeys();
-  }
-  return 0;
+    for (;;)
+    {
+        // Emulate a cycle
+        emulator.emulateCycle();
+
+        // if (emulator.drawFlag)
+        //   drawGraphics();
+
+        // emulator.setKeys();
+    }
+    return 0;
 }
